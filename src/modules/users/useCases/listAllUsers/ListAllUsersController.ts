bde608cfeae94d005091afbb3f2010d45159868b
German policarpo <fabriciopolicarpo0@gmail.com>
2022-10-23 22:23:27 -0400

    list

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+      const users = this.listAllUsersUseCase.execute({
+        user_id: user_id.toString(),
+      });
+      return response.status(201).json(users);
+    } catch (error) {
+      return response.status(error.status).json({ error: error.message });
+    }
   }
 }
 
